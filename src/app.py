f6392bd1915c461c932c6f4b08eaee596de6e8c7
Fenyx-Forte <fenyxforte@gmail.com>
2024-04-11 11:33:16 -0300

    adicionando as pastas config e logs, alem de adicionar logging em app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
+from logging import getLogger
+
+from logs import my_log
 from src.math_area import area
+
+module_name = "app"
+logger = getLogger(module_name)
 
 
 def main() -> None:
-    print(area.calculate_area_square(5))
+    logger.info(area.calculate_area_square(5))
